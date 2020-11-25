482f82204fed0805c5803f57125c626bcba643c8
conch2 <74294886+conch2@users.noreply.github.com>
2020-11-25 09:47:12 +0800

    Create c112502.QS排名查询.c

---
+++ c112502.QS排名查询.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	scanf("%d", &num);
+
+	if (num == 1)
+		printf("\n");
+	else if (num == 2)
+		printf("\n");
+	else if (num == 3)
+		printf("\n");
+	else if (num == 4)
+		printf("\n");
+	else if (num == 5)
+		printf("\n");
+	else if (num == 6)
+		printf("\n");
+	else if (num == 7)
+		printf("\n");
+	else if (num == 8)
+		printf("\n");
+	else if (num == 9)
+		printf("\n");
+	else if (num == 10)
+		printf("\n");
+	else
+		printf("老铁的排名太靠后了，请输入1-10\n");
+
+	return 0;
+}
