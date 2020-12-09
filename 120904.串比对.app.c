f3010626d83deed9ba73b71c23460d7d0459d23c
conch2 <74294886+conch2@users.noreply.github.com>
2020-12-09 16:50:21 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char array1[]="干饭人干饭魂，干饭都是人上人";
+	char array2[]="干饭人干饭魂，干饭都是人上人";
+
+	if(!strcmp(array1, array2))
+		printf("两个字符串是相同的。\n");
+	else 
+		printf("两个字符串是不相同的。\n");
+
+	return 0;
+}
